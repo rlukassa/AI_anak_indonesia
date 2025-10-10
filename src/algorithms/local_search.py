343f05262c451ce49c5a85ef2a38945ddada5111
Brian Ricardo Tamin <109451873+brii26@users.noreply.github.com>
2025-10-11 00:05:12 +0700

    Add local search abstract class

---
+++ src/algorithms/local_search.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from src.model.state import State
+
+class LocalSearch(ABC):
+      
+    @abstractmethod
+    def search(self, state: State, *objectives):
+        pass
