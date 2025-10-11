0ab6e7643904df7c4db0b6bdb8133210c82f192a
Brian Ricardo Tamin <109451873+brii26@users.noreply.github.com>
2025-10-11 17:15:17 +0700

    Feat: add hill climbing algorithm

--- src/algorithms/hill_climbing.py
+++ src/algorithms/hill_climbing.py
@@ -1,67 +1,129 @@
-from types import Any
+from dataclasses import dataclass
+from typing import Any
 from .local_search import LocalSearch
 from src.model.state import State
+import sys
+import random
 
+@dataclass
 class HillClimbing(LocalSearch):
-    
-    def __init__(self, variant: str = "steepest", max_iters: int = 1000, restarts: int = 10):
-        self.variant = variant
-        self.max_iters = max_iters
-        self.restarts = restarts
+    variant:str = "steepest"
          
-    def search(self, state: State, *objectives) -> Any: 
+    def search(self, state: State, *objectives, max_sideways=None, restarts=None, iterations=None) -> Any: 
         """ entry point search function """
         if self.variant == "steepest":
             return self._steepest(state, *objectives)
         if self.variant == "sideways":
-            return self._sideways(state, *objectives)
+            ms = sys.maxsize if max_sideways is None else max_sideways
+            return self._sideways(state, ms, *objectives)
         if self.variant == "stochastic":
-            return self._strochastic(state, *objectives)
+            iters = 10 if iterations is None else iterations
+            return self._stochastic(state, iters, *objectives)
         if self.variant == "random_restart":
-            return self._random_restart(state, *objectives)
+            rs = 10 if restarts is None else restarts
+            return self._random_restart(state, rs, *objectives)
+        else:
+            raise ValueError(f"Unknown algorithm type:{self.variant}")
+        
         
     def _steepest(self, state: State, *objectives) -> State: 
         """ steepest ascent hill-climbing  """
         current = state.copy()
         current.initialize_random_state(*objectives)
+        
         while True:
+            # generate successors
             successors = current.generate_all_successors(*objectives)
-            
             if not successors: 
-                return current
+                break
             
+            # compare value
             best_successor = max(successors, key=lambda s: s.state_value)
-            
             if best_successor.state_value <= current.state_value: 
-                return current
+                break
             current = best_successor
-            
-    def _sideways(self, state: State, *objectives) -> State:
-        """ sideways hill-climbing """
+        return current
+    
+    
+    def _sideways(self, state: State, MAX_SIDEWAYS: int, *objectives) -> State:
+        """ sideways variant hill-climbing """
+        def _keys(s: State):
+            return frozenset(s.assignments.items())
+        
+        current = state.copy()
+        current.initialize_random_state(*objectives)
+
+        visited = {_keys(current)}
+        sideways = 0
+
+        while True:
+            # generate successors
+            successors = current.generate_all_successors(*objectives)
+            if not successors:
+                break
+
+            current_value = current.state_value
+            best_value = max(s.state_value for s in successors)
+
+            # better value
+            if best_value > current_value:
+                better_states = [s for s in successors if s.state_value == best_value]
+                current = random.choice(better_states)
+                visited.add(_keys(current))
+                sideways = 0
+                continue
+
+            # same value
+            if best_value == current_value and sideways < MAX_SIDEWAYS:
+                equal_unvisited = [
+                    s for s in successors
+                    if s.state_value == current_value and _keys(s) not in visited
+                ]
+                if equal_unvisited:
+                    current = random.choice(equal_unvisited)
+                    visited.add(_keys(current))
+                    sideways += 1
+                    continue
+            break
+        return current
+    
+    
+    def _stochastic(self, state: State, iterations:int, *objectives) -> State:
+        """ stochastic hill-climbing """
         current = state.copy()
         current.initialize_random_state(*objectives)
         
-        visited = set()
-        visited.add(frozenset(current.assignments.items()))
-        
-        while True: 
-            successors = current.generate_all_successors(*objectives)
-            if not successors: 
-                return current
+        for _ in range(iterations):
+            # generate random successor
+            successor = current.generate_random_successor(*objectives)
             
-            found = False
-            for s in successors:
-                skey = frozenset(s.assignments.items())
-                if skey in visited or s.state_value < current.state_value:
-                    continue
-                current = s
-                visited.add(skey)
-                found = True
-                break
-            
-            if not found: 
-                return current
-                
+            # move if better
+            if successor.state_value > current.state_value:
+                current = successor 
+        return current
+    
+    
+    def _random_restart(self, state: State, restarts:int, *objectives) -> State:
+        """ random-restart hill-climbing """
+        best = None
         
+        for _ in range (restarts):
+            current = state.copy()
+            current.initialize_random_state(*objectives)
+            
+            while True:
+                # generate successors
+                successors = current.generate_all_successors(*objectives)
+                if not successors: 
+                    break
                 
+                # compare value
+                nxt = max(successors, key=lambda s:s.state_value)
+                if nxt.state_value <= current.state_value:
+                    break
+                current = nxt
                 
+            if best is None or current.state_value > best.state_value:
+                best = current
+                
+        return best.copy()
